90f4400a16040e9425e0f898c53df72af3413613
anoop <anoop@pacholakkal.com>
2025-05-17 17:13:01 -0700

    added log

--- myfib/fib.go
+++ myfib/fib.go
@@ -1,10 +1,13 @@
 package myfib
 
+import "fmt"
+
 func Fib(n int) int {
 	fib := []int{0, 1}
 
 	for i := 2; i <= n; i++ {
 		fib = append(fib, fib[i-1]+fib[i-2])
 	}
+	fmt.Println("fib: ", fib)
 	return fib[n]
 }
